32ca1898660dbf48159bcb8848295fb55bfd09c8
KingTalu <fitzroyivan@gmail.com>
2020-03-19 14:03:33 +1300

    2 files added

---
+++ src/Documentation.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+ 
+class Documentation extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+ 
+export default Documentation;
