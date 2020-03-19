12f28c95afef4e3408f313b4b8aca7af9cf3de05
KingTalu <fitzroyivan@gmail.com>
2020-03-19 13:57:14 +1300

    8 files added, 5 deleted

---
+++ src/Main.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Reminder from "./Reminder";
+import Documentation from "./Documentation";
+
+class Main extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/reminder">Reminder</NavLink></li>
+            <li><NavLink to="/documentation">Documentation</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/reminder" component={Reminder}/>
+            <Route path="/documentation" component={Documentation}/>
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+ 
+export default Main;
