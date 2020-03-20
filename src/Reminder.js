edf1e47ef6ba49e61f5c37f47ad13a25c195ff21
KingTalu <fitzroyivan@gmail.com>
2020-03-20 15:01:45 +1300

    1 file modified, 3 added, 2 deleted

--- src/Reminder.js
+++ src/Reminder.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import TodoList from "./components/App";
+import TodoList from "./components/TodoList";
  
 class Reminder extends Component {
   render() {
